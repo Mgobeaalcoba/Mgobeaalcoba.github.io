e9270b9e700ab2f2dd77807c63b7d37011d02004
Mgobeaalcoba <Mgobeaalcoba@users.noreply.github.com>
2026-09-24 14:00:23 +0000

    chore: deploy all sites 87c8eccf99bbefb853b89104358a440d58ab4b73 87c8eccf99bbefb853b89104358a440d58ab4b73

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'mga-shell-v1';
+const SHELL = ['/', '/portfolio/', '/blog/', '/recursos/', '/offline/', '/images/favicon-192.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (request.method !== 'GET' || url.origin !== self.location.origin || url.pathname.startsWith('/api/')) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(fetch(request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(request, copy));
+      return response;
+    }).catch(async () => (await caches.match(request)) || caches.match('/offline/')));
+    return;
+  }
+
+  event.respondWith(caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+    if (response.ok) caches.open(CACHE).then((cache) => cache.put(request, response.clone()));
+    return response;
+  })));
+});
